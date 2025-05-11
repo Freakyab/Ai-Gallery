7efffbe449090a7e97c191a00fd5beccf8c1ccec
Freakyab <aryanbhisikar1@gmail.com>
2025-05-11 13:40:49 +0530

    feat: update backend URL logic to include versioning in the link

--- app/backend.ts
+++ app/backend.ts
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NEXT_PUBLIC_DEVELOPMENT === 'true';
 const getVersion = process.env.NEXT_PUBLIC_VERSION || '0.0';
-const versionBackendLink = "https://ai-gallery-backend-v2.vercel.app"
+const versionBackendLink = "https://ai-gallery-backend-" + getVersion + ".vercel.app"
 export const backendUrl = isDevelopment ? 'http://localhost:8000' :
     (getVersion !== "master" && getVersion !== "main") ?
         versionBackendLink : 'https://ai-gallery-backend.vercel.app';
